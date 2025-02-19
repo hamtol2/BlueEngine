8dc977fcb19bd124b441741aa6fe21473538d85d
ronniej <ronniej@naver.com>
2025-02-19 16:11:44 +0900

    텍스처 매핑 구현 / CSO 로드하도록 개선

--- Engine/Render/QuadMesh.cpp
+++ Engine/Render/QuadMesh.cpp
@@ -1,6 +1,7 @@
 #include "QuadMesh.h"
 #include "Vertex.h"
 #include "../Shader/Shader.h"
+#include "Shader/TextureMappingShader.h"
 
 namespace Blue
 {
@@ -19,6 +20,6 @@ namespace Blue
 		std::vector<uint32> indices = { 0, 1, 3, 1, 2, 3 };
 
 		meshes.emplace_back(std::make_shared<MeshData>(vertices, indices));
-		shaders.emplace_back(std::make_shared<Shader>());
+		shaders.emplace_back(std::make_shared<TextureMappingShader>("T_coord.png"));
 	}
 }
